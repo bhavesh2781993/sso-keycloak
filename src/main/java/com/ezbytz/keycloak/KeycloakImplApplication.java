ec016d085866ea5a35fa77e238014f3111002f42
Bhavesh <shah27bhavesh@gmail.com>
2023-09-24 18:13:27 +0530

    Initial Commit

---
+++ src/main/java/com/ezbytz/keycloak/KeycloakImplApplication.java
@@ -0,0 +1,13 @@
+package com.ezbytz.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezbytz/keycloak/KeycloakImplApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ezbytz.keycloak;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
