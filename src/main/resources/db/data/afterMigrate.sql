874d6166fe1233afffd9771cea59557ae6173951
Bhavesh <shah27bhavesh@gmail.com>
2023-09-27 11:32:03 +0530

    flyway ddl + dml afterMigrate script

---
+++ src/main/resources/db/data/afterMigrate.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.keycloak_realms_lookup
+    VALUES ('TEST', 'http://localhost:9191/realms/Test', '{"jwksUri": "http://localhost:9191/realms/Test/protocol/openid-connect/certs"}');
